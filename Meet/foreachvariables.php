a1c4359106c232c48767b1f7bca5ffbd303f094e
Meet Parmar <ywcameet22@gmail.com>
2023-11-27 11:22:34 +0530

    Add files via upload

---
+++ Meet/foreachvariables.php
@@ -0,0 +1,10 @@
+<?php
+echo "Welcome to the world of foreach loops <br>";
+$arr = array("Bananas", "Apples", "Harpic", "Bread", "Butter");
+
+
+foreach ($arr as  $value) {
+    echo "$value <br>";
+}
+
+?>
